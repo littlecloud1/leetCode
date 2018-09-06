23922f072416f41376be51b7220ba1d3cd9a51fd
littlecloud1 <cloudtang030@gmail.com>
2018-09-06 17:56:53 +0800

    docs: fb clone graph.

---
+++ lc133.cpp
@@ -0,0 +1,30 @@
+//leetcode:133. Clone Graph
+//dfs O(n+m)
+//make a copy for every node
+//use map to prevent duplicate
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> mp;
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if (!node) return NULL;
+        
+        //create node
+        if (mp.find(node) == mp.end()){ 
+            mp[node] = new UndirectedGraphNode(node->label);
+            //check neighbour, use dfs
+            for(auto n:node->neighbors){
+                mp[node]->neighbors.push_back(cloneGraph(n));
+            }
+        }
+        return mp[node];
+    }
+};
