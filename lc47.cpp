43127c8f2953f8177cc03cf47bf5245885519ff7
littlecloud1 <cloudtang030@gmail.com>
2018-09-08 11:38:07 +0800

    docs: improve complexity by compare previous element.

--- lc47.cpp
+++ lc47.cpp
@@ -39,37 +39,35 @@ public:
     }
 };
 //improve:
-//because nums is sorted, we only have to check if nums[i] == nums[i-1] and used
+//because nums is sorted, we only have to check if nums[i] == nums[i-1] and previous one used
 //if it is the same and not used, then skip
 class Solution {
 public:
-    unordered_map<string, int> hash;
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<int> sub;
-        vector<vector<int>> res;
-        if (nums.empty()) return res;
-        bool used[nums.size()] = {false}; //make sure to initialize bool
-        sort(nums.begin(), nums.end());
-        dfs(res, sub, nums, used, "");
-        
-        return res;
-    }
-        
-    void dfs (vector<vector<int>>& res, vector<int>& sub, vector<int>& nums, bool used[], string s){
-        if (sub.size() == nums.size() && hash.find(s) == hash.end()){
-            res.push_back(sub);
-            hash[s] = 1;
-            return;
-        }
-        for (int i =0; i < nums.size(); i++){
-            if(!used[i]){
-                used[i] = true;
-                sub.push_back(nums[i]);
-                string sub_s = s + to_string(nums[i]);
-                dfs(res, sub, nums, used, sub_s);
-                sub.pop_back();
-                used[i] = false;
-            }
-        }
-    }
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+		vector<int> sub;
+		vector<vector<int>> res;
+		if (nums.empty()) return res;
+		bool used[nums.size()] = { false }; //make sure to initialize bool
+		sort(nums.begin(), nums.end());
+		dfs(res, sub, nums, used);
+
+		return res;
+	}
+
+	void dfs(vector<vector<int>>& res, vector<int>& sub, vector<int>& nums, bool used[]) {
+		if (sub.size() == nums.size()) {
+			res.push_back(sub);
+			return;
+		}
+		for (int i = 0; i < nums.size(); i++) {
+			if (!used[i]) {
+				if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) continue;
+				used[i] = true;
+				sub.push_back(nums[i]);
+				dfs(res, sub, nums, used);
+				sub.pop_back();
+				used[i] = false;
+			}
+		}
+	}
 };
